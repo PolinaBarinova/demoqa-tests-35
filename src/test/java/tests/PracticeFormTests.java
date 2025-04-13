ecf75018812192741b76872e09c9cdc1f3a81cc6
Polina Barinova <barinova_polina@list.ru>
2025-04-13 20:44:31 +0300

    autotest fill practice-form

---
+++ src/test/java/tests/PracticeFormTests.java
@@ -0,0 +1,59 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class PracticeFormTests {
+
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void fillFormTest() {
+        open("/automation-practice-form");
+
+        executeJavaScript("$('footer').remove();");
+        executeJavaScript("$('#fixedban').remove();");
+
+        $("#firstName").setValue("Alina");
+        $("#lastName").setValue("Inova");
+        $("#userEmail").setValue("test@test.ru");
+        $("#genterWrapper").$(byText("Female")).click();
+        $("#userNumber").setValue("7776665544");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("May");
+        $(".react-datepicker__year-select").selectOption("2005");
+        $$("div.react-datepicker__day").findBy(text("3")).click();
+        $("#subjectsInput").setValue("Maths").pressEnter();
+        $("#hobbiesWrapper").$(byText("Music")).click();
+        $("#uploadPicture").uploadFromClasspath("mem-kot.jpg");
+        $("#currentAddress").setValue("Some address");
+        $("#state").click();
+        $("#react-select-3-option-0").click();
+        $("#city").click();
+        $("#react-select-4-option-0").click();
+        $("#submit").click();
+
+        $(".table-responsive").shouldBe(visible);
+        $(".table-responsive").$(byText("Student Name")).parent().shouldHave(text("Alina Inova"));
+        $(".table-responsive").$(byText("Student Email")).parent().shouldHave(text("test@test.ru"));
+        $(".table-responsive").$(byText("Gender")).parent().shouldHave(text("Female"));
+        $(".table-responsive").$(byText("Mobile")).parent().shouldHave(text("7776665544"));
+        $(".table-responsive").$(byText("Date of Birth")).parent().shouldHave(text("03 May,2005"));
+        $(".table-responsive").$(byText("Subjects")).parent().shouldHave(text("Maths"));
+        $(".table-responsive").$(byText("Hobbies")).parent().shouldHave(text("Music"));
+        $(".table-responsive").$(byText("Picture")).parent().shouldHave(text("mem-kot.jpg"));
+        $(".table-responsive").$(byText("Address")).parent().shouldHave(text("Some address"));
+        $(".table-responsive").$(byText("State and City")).parent().shouldHave(text("NCR Delhi"));
+    }
+}

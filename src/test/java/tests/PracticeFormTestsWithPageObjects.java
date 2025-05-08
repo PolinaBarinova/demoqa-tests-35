1052dbf5e14bb34a66a8c2f1b49b12ce5999a36c
Polina Barinova <barinova_polina@list.ru>
2025-05-08 18:25:55 +0300

    added edits to method names and line alignment

--- src/test/java/tests/PracticeFormTestsWithPageObjects.java
+++ src/test/java/tests/PracticeFormTestsWithPageObjects.java
@@ -25,72 +25,64 @@ public class PracticeFormTestsWithPageObjects {
     String userState = getRandomState();
     String userCity = getRandomCityByState(userState);
 
-
-
-        @BeforeAll
-        static void prepareEnvironment(){
-            Configuration.browserSize = "1920x1080";
-            Configuration.baseUrl = "https://demoqa.com";
-            Configuration.pageLoadStrategy = "eager";
-        }
-
-        @Test
-        void fillFormTest() {
-
-            registrationPage.openPage()
-                    .removeBanners()
-                    .setFirstName(firstName)
-                    .setLastName(lastName)
-                    .setEmail(userEmail)
-                    .setGender(userGender)
-                    .setUserNumber(userNumber)
-                    .setDateOfBirth(dayOfBirth, monthOfBirth, yearOfBirth)
-                    .setSubjects(userSubject)
-                    .setHobbies(userHobby)
-                    .uploadPicture()
-                    .setAddress(userAddress)
-                    .setState(userState)
-                    .setCity(userCity)
-                    .submitForm()
-                    .checkResultTableVisibility()
-                    .checkResult("Student Name", firstName + " " + lastName)
-                    .checkResult("Student Email", userEmail)
-                    .checkResult("Gender", userGender)
-                    .checkResult("Mobile", userNumber)
-                    .checkResult("Date of Birth", dayOfBirth + " " + monthOfBirth + "," + yearOfBirth)
-                    .checkResult("Subjects", userSubject)
-                    .checkResult("Hobbies", userHobby)
-                    .checkResult("Picture", "mem-kot.jpg")
-                    .checkResult("Address", userAddress)
-                    .checkResult("State and City", userState + " " + userCity);
-        }
-
-        @Test
-
-        void fillOnlyRequiredFields () {
-
-            registrationPage.openPage()
-                    .removeBanners()
-                    .setFirstName(firstName)
-                    .setLastName(lastName)
-                    .setGender(userGender)
-                    .setUserNumber(userNumber)
-                    .submitForm()
-                    .checkResultTableVisibility()
-                    .checkResult("Student Name", firstName + " " + lastName)
-                    .checkResult("Gender", userGender)
-                    .checkResult("Mobile", userNumber);
-
-        }
-
-        @Test
-
-        void submitEmptyForm () {
-
-            registrationPage.openPage()
-                    .submitForm()
-                    .checkResultTableUnvisibility();
-        }
+    @BeforeAll
+    static void prepareEnvironment() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
     }
+
+    @Test
+    void fillFormTest() {
+        registrationPage.openPage()
+                .removeBanners()
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setEmail(userEmail)
+                .setGender(userGender)
+                .setUserNumber(userNumber)
+                .setDateOfBirth(dayOfBirth, monthOfBirth, yearOfBirth)
+                .setSubjects(userSubject)
+                .setHobbies(userHobby)
+                .uploadPicture()
+                .setAddress(userAddress)
+                .setState(userState)
+                .setCity(userCity)
+                .submitForm()
+                .checkResultTableVisibility()
+                .checkResult("Student Name", firstName + " " + lastName)
+                .checkResult("Student Email", userEmail)
+                .checkResult("Gender", userGender)
+                .checkResult("Mobile", userNumber)
+                .checkResult("Date of Birth", dayOfBirth + " " + monthOfBirth + "," + yearOfBirth)
+                .checkResult("Subjects", userSubject)
+                .checkResult("Hobbies", userHobby)
+                .checkResult("Picture", "mem-kot.jpg")
+                .checkResult("Address", userAddress)
+                .checkResult("State and City", userState + " " + userCity);
+    }
+
+    @Test
+    void fillOnlyRequiredFieldsTest() {
+        registrationPage.openPage()
+                .removeBanners()
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setGender(userGender)
+                .setUserNumber(userNumber)
+                .submitForm()
+                .checkResultTableVisibility()
+                .checkResult("Student Name", firstName + " " + lastName)
+                .checkResult("Gender", userGender)
+                .checkResult("Mobile", userNumber);
+    }
+
+    @Test
+    void submitEmptyFormTest() {
+        registrationPage.openPage()
+                .submitForm()
+                .checkResultTableUnvisibility();
+    }
+}
 
 
